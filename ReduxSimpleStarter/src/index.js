64da392e1c40405f558345024a0a01fb5b5971c0
Unknown <joimh@naver.com>
2018-05-22 22:13:38 +0900

    5-22 react 스터디 소스
    1. import Export
    2. 클래스 기반 컴포넌트
    3. youtube api

--- ReduxSimpleStarter/src/index.js
+++ ReduxSimpleStarter/src/index.js
@@ -1,13 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import Searchbar from './components/search_bar';
+
+const API_KEY = 'AIzaSyA2PK1IAHpWT--LEGuL24V8oxor4W6PCqo';
+
 //Create a new Component, this component should produce
 //some HTML
 
 const App = () => {
-    return <div>Hi! Test</div>;
+    return <div>
+        <Searchbar />
+    </div>;
 }
 
-ReactDOM.render(<App />, document.querySelector('.container'));
-
-
+ReactDOM.render(< App /> , document.querySelector('.container')); 
