306bf73c02b5d7a1d97ec402118489f033238618
Unknown <joimh@naver.com>
2018-05-24 02:05:46 +0900

    props 공부
    props 공부

--- ReduxSimpleStarter/src/index.js
+++ ReduxSimpleStarter/src/index.js
@@ -1,17 +1,29 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
-
+import YTSearch from 'youtube-api-search';
 import Searchbar from './components/search_bar';
-
+import VideoList from './components/video_list';
 const API_KEY = 'AIzaSyA2PK1IAHpWT--LEGuL24V8oxor4W6PCqo';
 
-//Create a new Component, this component should produce
-//some HTML
+class App extends Component {
+    constructor(props) {
+        super(props);
 
-const App = () => {
-    return <div>
-        <Searchbar />
-    </div>;
+        this.state = { videos: [] };
+
+        YTSearch({ key: API_KEY, term: 'surfboards' }, (videos) => {
+            this.setState({ videos });
+        })
+    }
+
+    render() {
+        return (
+            <div>
+                <Searchbar />
+                <VideoList videos = {this.state.videos} />
+            </div>
+        );
+    }
 }
 
 ReactDOM.render(< App /> , document.querySelector('.container')); 
