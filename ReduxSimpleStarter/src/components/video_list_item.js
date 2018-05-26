f290e715feb2a7b4c413088f4ee27c5c63371265
Unknown <joimh@naver.com>
2018-05-27 05:33:41 +0900

    list-item 출력 및 아이템 키
    list-item 출력 및 아이템 키

--- ReduxSimpleStarter/src/components/video_list_item.js
+++ ReduxSimpleStarter/src/components/video_list_item.js
@@ -1,7 +1,24 @@
 import React from 'react';
 
-const VideoListItem = (props) => {
-    return <li>Video</li>
+const VideoListItem = ({ video }) => {
+    //const video = props.video는
+    //위의 {video}와 같다.
+
+    const imageUrl = video.snippet.thumbnails.default.url;
+
+    return (
+        <li className="list-group-item">
+            <div className="video-list media">
+                <div className="media-left">
+                    <img className="media-object" src={imageUrl} /> 
+                </div>
+            </div>
+
+            <div className="media-body">
+                <div className="media-heading">{video.snippet.title}</div>
+            </div>
+        </li>
+    );
 }
 
 export default VideoListItem;
