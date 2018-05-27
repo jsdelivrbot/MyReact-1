a6bece50083a29c8e63d6bd2cfd6ee6efb7cea1b
Unknown <joimh@naver.com>
2018-05-28 02:22:50 +0900

    search input & add lodash
    search input & add lodash

--- ReduxSimpleStarter/src/index.js
+++ ReduxSimpleStarter/src/index.js
@@ -1,3 +1,5 @@
+
+import _ from 'lodash';
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import YTSearch from 'youtube-api-search';
@@ -15,8 +17,12 @@ class App extends Component {
             selectedVideo: null 
         };
 
-        YTSearch({ key: API_KEY, term: 'surfboards' }, (videos) => {
-            this.setState({ 
+        this.videoSearch('surfboards');
+   }
+
+    videoSearch(term) {
+        YTSearch({ key: API_KEY, term: term }, (videos) => {
+            this.setState({
                 videos: videos,
                 selectedVideo: videos[0]
             });
@@ -24,9 +30,11 @@ class App extends Component {
     }
 
     render() {
+        const videoSearch = _.debounce((term) => { this.videoSearch(term) }, 1000);
+
         return (
             <div>
-                <Searchbar />
+                <Searchbar onSearchTermChange={videoSearch} />
                 <VideoDetail video={this.state.selectedVideo} />
                 <VideoList 
                     onVideoSelect={selectedVideo => this.setState({ selectedVideo})}
