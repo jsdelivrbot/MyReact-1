ce61618d7cc47aed01a06364a38076909a3d4333
Unknown <joimh@naver.com>
2018-05-29 01:50:15 +0900

    Reducer
    Reducer

---
+++ ReduxSimpleStarter/src/reducers/reducer_books.js
@@ -0,0 +1,8 @@
+export default function() {
+    return [
+        { title: 'JavaScript: Goodpart' },
+        { title: 'Harry Poter'},
+        { title: 'Dark Tower'},
+        { title: 'Eloquent Ruby'}
+    ]
+}
