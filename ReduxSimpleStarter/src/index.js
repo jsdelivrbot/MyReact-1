fc651e5ffa3e5b9c783352e47c9e7e95091c7d5b
Unknown <joimh@naver.com>
2018-06-01 22:57:11 +0900

    react 리덕스
    react 리덕스

--- ReduxSimpleStarter/src/index.js
+++ ReduxSimpleStarter/src/index.js
@@ -2,12 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
-import ReduxPromise from 'redux-promise';
 
 import App from './components/app';
 import reducers from './reducers';
 
-const createStoreWithMiddleware = applyMiddleware(ReduxPromise)(createStore);
+// browserHistory는 url이 바뀔 떄 어떤 컴포넌트를 랜더링할지 결정
+// import { Router, browserHistory } from 'react-router';
+
+// hashHistory는 url이 바뀔 때 해쉬 심볼 이후의 것을 감지할 수 있도록 결정
+// import { Router, hashHistory } from 'react-router';
+
+const createStoreWithMiddleware = applyMiddleware()(createStore);
 
 ReactDOM.render(
   <Provider store={createStoreWithMiddleware(reducers)}>
