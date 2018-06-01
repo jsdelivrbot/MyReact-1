a6231c20dcfddee0bd92b65da36636b3ae9d180d
Unknown <joimh@naver.com>
2018-06-01 14:59:20 +0900

    미들웨어 마지막
    미들웨어 마지막 부분
    T.T 그러나 react-google-maps 쪽이 많이 구버전이라 현재 react에서 잘 동작하지 않아 부득이하게 테스트 못함

---
+++ ReduxSimpleStarter/src/components/google_map.js
@@ -0,0 +1,18 @@
+import React from 'react';
+//import { GoogleMap, GoogleMapLoader } from 'react-google-maps';
+
+export default (props) => {
+    return (
+        // <GoogleMapLoader 
+        //     containerElement={ <div style={{height: '100%'}} /> }
+        //     googleMapElement={
+        //         <GoogleMap
+        //             defaultZoom={8}
+        //             defaultCenter={{ lat: -34.397, lng: 150.644 }}
+        //         />
+        //     }
+        // />
+        <div></div>
+    )
+}
+
