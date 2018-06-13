f030ce9dc9297d7742e53c6c05415e180caf2096
Unknown <joimh@naver.com>
2018-06-13 22:57:42 +0900

    redux-counter 공부
    redux-counter 공부

---
+++ redux-form-test/src/containers/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import ContactForm from '../components/index';
+
+class ContactPage extends React.Component {
+    submit = values => {
+        // print the form values to the console
+        console.log(values)
+    }
+    render() {
+        return <ContactForm onSubmit={this.submit} />
+    }
+}
+
+export default ContactPage;
+
